cf432d375b381e0181f61562e1ada1774ee31c10
kerakera000 <leon51414@icloud.com>
2023-05-15 14:51:32 +0900

    vue のコード移行

---
+++ components/new-solvide-service/NewserviceFlowComp.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="NSFlow">
+        <div class="NSFlow__title">
+            <div class="Ntitle">
+                <h2>BLOGS</h2>
+                <p>ブログ記事</p>
+                <span></span>
+            </div>
+        </div>
+
+        <div class="NSFlow__content">
+            <div class="NSFlow__content--scroll-box">
+                <div class="NSFlow-item">
+                    <h3>01</h3>
+                    <div class="NSFlow-item--img">
+                        <img src="../../assets/new-solvide-images/service-images/flow/flow1.png" alt="">
+                    </div>
+                </div>
+                <div class="NSFlow-item-text1">
+                    <p>お問い合わせフォーム<br>からご依頼</p>
+                </div>
+            </div>
+
+            <div class="NSFlow__content--scroll-box">
+                <div class="NSFlow-item">
+                    <h3>02</h3>
+                    <div class="NSFlow-item--img">
+                        <img src="../../assets/new-solvide-images/service-images/flow/flow2.png" alt="">
+                    </div>
+                </div>
+                <div class="NSFlow-item-text2">
+                    <p>お客様のご要望、<br>問題をヒアリング</p>
+                </div>
+            </div>
+
+            <div class="NSFlow__content--scroll-box">
+                <div class="NSFlow-item">
+                    <h3>03</h3>
+                    <div class="NSFlow-item--img">
+                        <img src="../../assets/new-solvide-images/service-images/flow/flow3.png" alt="">
+                    </div>
+                </div>
+                <div class="NSFlow-item-text3">
+                    <p>お見積もりの作成</p>
+                </div>
+            </div>
+
+            <div class="NSFlow__content--scroll-box">
+                <div class="NSFlow-item">
+                    <h3>04</h3>
+                    <div class="NSFlow-item--img">
+                        <img src="../../assets/new-solvide-images/service-images/flow/flow4.png" alt="">
+                    </div>
+                </div>
+                <div class="NSFlow-item-text4">
+                    <p>制作開始</p>
+                </div>
+            </div>
+
+            <div class="NSFlow__content--scroll-box">
+                <div class="NSFlow-item">
+                    <h3>05</h3>
+                    <div class="NSFlow-item--img">
+                        <img src="../../assets/new-solvide-images/service-images/flow/flow5.png" alt="">
+                    </div>
+                </div>
+                <div class="NSFlow-item-text5">
+                    <p>最終確認</p>
+                </div>
+            </div>
+
+            <div class="NSFlow__content--scroll-box">
+                <div class="NSFlow-item">
+                    <h3>06</h3>
+                    <div class="NSFlow-item--img">
+                        <img src="../../assets/new-solvide-images/service-images/flow/flow6.png" alt="">
+                    </div>
+                </div>
+                <div class="NSFlow-item-text6">
+                    <p>納品/お支払い</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'NServiceFlowComp',
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/service.vue
@@ -0,0 +1,26 @@
+<template>
+    <NServiceComp />
+    <NServicePointComp />
+    <NServiceContentComp />
+    <NServiceFlowComp />
+    <NContactComp />
+</template>
+
+<script>
+import NContactComp from '@/components/solvide/NContactComp.vue';
+import NServiceComp from '@/components/new-solvide-service/NewserviceKVComp.vue';
+import NServiceFlowComp from '@/components/new-solvide-service/NewserviceFlowComp.vue';
+import NServicePointComp from '@/components/new-solvide-service/NservicePointComp.vue';
+import NServiceContentComp from '@/components/new-solvide-service/NewserviceContentComp.vue';
+
+export default {
+    name: 'Home-page',
+    components: {
+        NContactComp,
+        NServiceContentComp,
+        NServiceFlowComp,
+        NServiceComp,
+        NServicePointComp,
+    },
+};
+</script>
